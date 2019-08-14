e6ba3782d1522cdfbe19f28626990851a7557ab9
Paulo Portella <paulo.portella@radicalcompany.com>
2019-08-14 16:13:25 +0100

    add . config files

---
+++ .rescriptsrc.js
@@ -0,0 +1,34 @@
+const rewireStyledComponents = require("react-app-rewire-styled-components")
+const rewireStyledComponentsTypescriptPlugin = require("react-app-rewire-styled-components-typescript")
+
+module.exports = {
+  webpack: (config, env) => {
+    config = rewireStyledComponents(config, env)
+    config = rewireStyledComponentsTypescriptPlugin(config, env)
+
+    return config
+  },
+  jest: config => {
+    const prevTestExtensions = config.moduleFileExtensions
+    const newConfig = {
+      ...config,
+      setupFiles: ["<rootDir>/test/jest-config/setup.js"],
+      testURL: "http://localhost",
+      collectCoverageFrom: ["src/**/*.{ts,tsx,js,jsx,mjs}"],
+      testEnvironment: "node",
+      transformIgnorePatterns: [
+        "<rootDir>/node_modules/(?!(lodash-es|react-apollo)/).+\\.(mjs|js|jsx|ts|tsx)$"
+      ],
+      transform: {
+        ...config.transform,
+        "^.+\\.(graphql|gql)$": "jest-transform-graphql"
+      },
+      moduleFileExtensions: [...prevTestExtensions],
+      moduleNameMapper: {
+        "^@[/](.+)": "<rootDir>/src/$1",
+        "^components[/](.+)": "<rootDir>/src/components/$1"
+      }
+    }
+    return newConfig
+  }
+}
